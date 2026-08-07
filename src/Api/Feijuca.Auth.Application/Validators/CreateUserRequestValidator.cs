61dc155696a554e4c3c4eb41ea06ef7082f9475c
Gabriel <henrique.biel9753@gmail.com>
2026-08-07 19:03:13 -0300

    fix: resolve Keycloak scopes setup crash and add admin email validation

---
+++ src/Api/Feijuca.Auth.Application/Validators/CreateUserRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using Feijuca.Auth.Application.Requests.User;
+using FluentValidation;
+
+namespace Feijuca.Auth.Application.Validators;
+
+public class CreateUserRequestValidator : AbstractValidator<AddUserRequest>
+{
+    public CreateUserRequestValidator()
+    {
+        RuleFor(x => x.Username)
+            .NotEmpty().WithMessage("The admin username is required.")
+            .EmailAddress().WithMessage("The admin username must be a valid email address.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("The password is required.")
+            .MinimumLength(6).WithMessage("The password must be at least 6 characters long.");
+    }
+}

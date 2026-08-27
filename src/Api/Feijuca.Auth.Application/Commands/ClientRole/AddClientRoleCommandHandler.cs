16ab6a845deaa0c9798daa1b1f35ae2d48d2c24c
Gabriel <henrique.biel9753@gmail.com>
2026-08-27 11:19:22 -0300

    fix: Ajustes

--- src/Api/Feijuca.Auth.Application/Commands/ClientRole/AddClientRoleCommandHandler.cs
+++ src/Api/Feijuca.Auth.Application/Commands/ClientRole/AddClientRoleCommandHandler.cs
@@ -23,10 +23,12 @@ public class AddClientRoleCommandHandler(IClientRoleRepository clientRolesReposi
             return Result<bool>.Failure(RoleErrors.AddRoleErrors);
         }
 
+        var result = await AddClientRoleAsync(request.AddClientRolesRequest.ClientRoles, tenantProvider.Tenant.Name, cancellationToken);
+
         if (request.AddClientRolesRequest.AllTenants)
         {
             var realms = await realmRepository.GetAllAsync(cancellationToken);
-            var tenants = realms.Select(r => r.Realm);
+            var tenants = realms.Where(r => r.Realm != tenantProvider.Tenant.Name).Select(r => r.Realm);
 
             foreach (var tenant in tenants)
             {
@@ -42,8 +44,6 @@ public class AddClientRoleCommandHandler(IClientRoleRepository clientRolesReposi
 
             return Result<bool>.Success(true);
         }
-
-        var result = await AddClientRoleAsync(request.AddClientRolesRequest.ClientRoles, tenantProvider.Tenant.Name, cancellationToken);
 
         return result;
     }

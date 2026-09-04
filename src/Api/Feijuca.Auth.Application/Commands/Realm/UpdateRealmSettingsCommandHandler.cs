938927a14e8fa83110cecc719d2c3c2a64b7e549
Gabriel <henrique.biel9753@gmail.com>
2026-09-04 11:37:03 -0300

    feat: Adiciona campos de tempo de vida em Realm e comandos
    Inclui novos campos de configuração de tempo de vida de tokens e sessões na entidade RealmEntity, mapeamentos e repositórios. Cria comando e handler para atualização dessas configurações. Ajusta replicação de realms para incluir esses campos e realiza pequenas melhorias de organização no código.

---
+++ src/Api/Feijuca.Auth.Application/Commands/Realm/UpdateRealmSettingsCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Feijuca.Auth.Application.Mappers;
+using Feijuca.Auth.Domain.Interfaces;
+using Feijuca.Auth.Models;
+using Feijuca.Auth.Providers;
+using LiteBus.Commands.Abstractions;
+
+namespace Feijuca.Auth.Application.Commands.Realm;
+
+public class UpdateRealmSettingsCommandHandler(ITenantProvider tenantProvider,
+    IRealmRepository realmRepository) : ICommandHandler<UpdateRealmSettingsCommand, Result>
+{
+    public async Task<Result> HandleAsync(UpdateRealmSettingsCommand command, CancellationToken cancellationToken = default)
+    {
+        var realm = await realmRepository.GetAsync(tenantProvider.Tenant.Name, cancellationToken);
+
+        var newRealm = realm.Data.ToUpdateSettings(command.Request);
+
+        var result = await realmRepository.UpdateRealmAsync(tenantProvider.Tenant.Name, newRealm, cancellationToken);
+
+        if (!result.IsSuccess)
+        {
+            return Result.Failure(result.Error);
+        }
+
+        return Result.Success();
+    }
+}
--- src/Api/Feijuca.Auth.Application/Mappers/RealmMapper.cs
+++ src/Api/Feijuca.Auth.Application/Mappers/RealmMapper.cs
@@ -1,4 +1,5 @@
-﻿using Feijuca.Auth.Application.Responses;
+﻿using Feijuca.Auth.Application.Requests.Realm;
+using Feijuca.Auth.Application.Responses;
 using Feijuca.Auth.Domain.Entities;
 using Feijuca.Auth.Http.Requests;
 using Flurl;
@@ -15,7 +16,13 @@ namespace Feijuca.Auth.Application.Mappers
                 DisplayName = addRealmRequest.Description,
                 Enabled = true,
                 Attributes = [],
-                BrowserSecurityHeaders = []
+                BrowserSecurityHeaders = [],
+                AccessTokenLifespan = (int)TimeSpan.FromHours(1).TotalSeconds,
+                AccessTokenLifespanForImplicitFlow = (int)TimeSpan.FromMinutes(15).TotalSeconds,
+                SsoSessionIdleTimeout = (int)TimeSpan.FromMinutes(30).TotalSeconds,
+                SsoSessionMaxLifespan = (int)TimeSpan.FromHours(10).TotalSeconds,
+                OfflineSessionIdleTimeout = (int)TimeSpan.FromDays(30).TotalSeconds,
+                OfflineSessionMaxLifespan = (int)TimeSpan.FromDays(60).TotalSeconds
             };
         }
 
@@ -29,6 +36,17 @@ namespace Feijuca.Auth.Application.Mappers
                     r.Enabled,
                     r.Attributes
                     ));
+        }
+
+        public static RealmEntity ToUpdateSettings(this RealmEntity realm, UpdateRealmSettingsRequest request)
+        {
+            realm.AccessTokenLifespan = request.AccessTokenLifespan ?? realm.AccessTokenLifespan;
+            realm.AccessTokenLifespanForImplicitFlow = request.AccessTokenLifespanForImplicitFlow ?? realm.AccessTokenLifespanForImplicitFlow;
+            realm.SsoSessionIdleTimeout = request.SsoSessionIdleTimeout ?? realm.SsoSessionIdleTimeout;
+            realm.SsoSessionMaxLifespan = request.SsoSessionMaxLifespan ?? realm.SsoSessionMaxLifespan;
+            realm.OfflineSessionIdleTimeout = request.OfflineSessionIdleTimeout ?? realm.OfflineSessionIdleTimeout;
+            realm.OfflineSessionMaxLifespan = request.OfflineSessionMaxLifespan ?? realm.OfflineSessionMaxLifespan;
+            return realm;
         }
     }
 }

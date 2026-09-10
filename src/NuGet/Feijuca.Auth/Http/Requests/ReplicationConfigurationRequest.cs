4b4a0cb477f8669223f48978a79551a74cb5f158
Gabriel <henrique.biel9753@gmail.com>
2026-09-10 12:57:27 -0300

    Adiciona IncludeGroups em ReplicationConfigurationRequest
    Agora é possível incluir grupos na configuração de replicação através da nova propriedade IncludeGroups adicionada ao record ReplicationConfigurationRequest.

--- src/NuGet/Feijuca.Auth/Http/Requests/ReplicationConfigurationRequest.cs
+++ src/NuGet/Feijuca.Auth/Http/Requests/ReplicationConfigurationRequest.cs
@@ -5,5 +5,6 @@ public record ReplicationConfigurationRequest(
     bool IncludeClientRoles,
     bool IncludeClientScopes,
     bool CreateAdminGroupWithAllRulesAssociated,
+    bool IncludeGroups,
     LoginUserRequest AdminUser
     );

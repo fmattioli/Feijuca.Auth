2d9addf68de36dcdd04588a4f1c171b4b7c1ab35
Felipe Mattioli Dos Santos (MSC Technology Italia) <felipe.mattiolidossantos@msc.com>
2026-09-14 11:48:42 +0200

    FEAT: Adding changes

--- src/Api/Feijuca.Auth.Application/Commands/Client/AddClientCommandHandler.cs
+++ src/Api/Feijuca.Auth.Application/Commands/Client/AddClientCommandHandler.cs
@@ -6,59 +6,58 @@ using Feijuca.Auth.Providers;
 using LiteBus.Commands.Abstractions;
 using Microsoft.Extensions.Logging;
 
-namespace Feijuca.Auth.Application.Commands.Client
+namespace Feijuca.Auth.Application.Commands.Client;
+
+public class AddClientCommandHandler(IRealmRepository realmRepository,
+    IClientRepository clientRepository,
+    ITenantProvider tenantService,
+    ILogger<AddClientCommandHandler> logger) : ICommandHandler<AddClientCommand, Result>
 {
-    public class AddClientCommandHandler(IRealmRepository realmRepository,
-        IClientRepository clientRepository,
-        ITenantProvider tenantService,
-        ILogger<AddClientCommandHandler> logger) : ICommandHandler<AddClientCommand, Result>
+    public async Task<Result> HandleAsync(AddClientCommand request, CancellationToken cancellationToken = default)
     {
-        public async Task<Result> HandleAsync(AddClientCommand request, CancellationToken cancellationToken)
+        var client = request.AddClientRequest.ToClientEntity();
+
+        var result = await clientRepository.CreateClientAsync(client, tenantService.Tenant.Name, cancellationToken);
+
+        if (!result.IsSuccess)
         {
-            var client = request.AddClientRequest.ToClientEntity();
+            logger.LogError("Failed to create client in tenant: {Tenant}. Error: {Error}", tenantService.Tenant.Name, result.Error);
+            return Result.Failure(result.Error);
+        }
 
-            var result = await clientRepository.CreateClientAsync(client, tenantService.Tenant.Name, cancellationToken);
+        if (request.AddClientRequest.AllTenants)
+        {
+            var realms = await realmRepository.GetAllAsync(cancellationToken);
 
-            if (!result.IsSuccess)
+            var tenants = realms
+                .Where(r => r.Realm != tenantService.Tenant.Name)
+                .Select(r => r.Realm);
+
+            foreach (var tenant in tenants)
             {
-                logger.LogError("Failed to create client in tenant: {Tenant}. Error: {Error}", tenantService.Tenant.Name, result.Error);
-                return Result.Failure(result.Error);
-            }
+                var clients = await clientRepository.GetClientsAsync(tenant, cancellationToken);
 
-            if (request.AddClientRequest.AllTenants)
-            {
-                var realms = await realmRepository.GetAllAsync(cancellationToken);
-
-                var tenants = realms
-                    .Where(r => r.Realm != tenantService.Tenant.Name)
-                    .Select(r => r.Realm);
-
-                foreach (var tenant in tenants)
+                if (clients.Data.Any(x => x.ClientId == request.AddClientRequest.ClientId))
                 {
-                    var clients = await clientRepository.GetClientsAsync(tenant, cancellationToken);
-
-                    if (clients.Data.Any(x => x.ClientId == request.AddClientRequest.ClientId))
-                    {
-                        logger.LogWarning("Client already exists in tenant: {Tenant}", tenant);
-                        continue;
-                    }
-
-                    var response = await clientRepository.CreateClientAsync(client, tenant, cancellationToken);
-
-                    if (!response.IsSuccess)
-                    {
-                        logger.LogError("Failed to create client in tenant: {Tenant}. Error: {Error}", tenant, response.Error);
-                        continue;
-                    }
-
-                    logger.LogInformation("Creating client in tenant: {Tenant}", tenant);
+                    logger.LogWarning("Client already exists in tenant: {Tenant}", tenant);
+                    continue;
                 }
 
-                return Result.Success();
+                var response = await clientRepository.CreateClientAsync(client, tenant, cancellationToken);
+
+                if (!response.IsSuccess)
+                {
+                    logger.LogError("Failed to create client in tenant: {Tenant}. Error: {Error}", tenant, response.Error);
+                    continue;
+                }
+
+                logger.LogInformation("Creating client in tenant: {Tenant}", tenant);
             }
 
-            logger.LogInformation("Creating client in tenant: {Tenant}", tenantService.Tenant.Name);
             return Result.Success();
         }
+
+        logger.LogInformation("Creating client in tenant: {Tenant}", tenantService.Tenant.Name);
+        return Result.Success();
     }
 }
